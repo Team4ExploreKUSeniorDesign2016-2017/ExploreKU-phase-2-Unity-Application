40cd2a097da4ee84cbe9bb4e71ca4acfefdf85c8
Tommy Li <h403l224@ku.edu>
2017-01-18 20:04:08 -0600

    Init

---
+++ Assets/Scripts/Billboard/Billboard.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Billboard : MonoBehaviour {
+
+	Camera mainCamera;
+
+	void Start()
+	{
+		mainCamera = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.forward = -mainCamera.transform.forward;
+	}
+}
---
+++ Assets/Scripts/PhoneInput/PhoneCameraInput.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PhoneCameraInput : MonoBehaviour {
+
+	public RawImage PlaySpace;
+	public RectTransform rectTransform;
+	public Text ResolutionOutput;
+
+	WebCamTexture inputTexture;
+
+	IEnumerator Start() 
+	{
+		yield return Application.RequestUserAuthorization(UserAuthorization.WebCam);
+
+		if (Application.HasUserAuthorization(UserAuthorization.WebCam)) 
+		{
+			inputTexture = new WebCamTexture (720, 1280);
+			PlaySpace.texture = inputTexture;
+
+			inputTexture.Play ();
+		}
+	}
+
+	void Update()
+	{
+		if (inputTexture) 
+		{
+			ResolutionOutput.text = "X" + inputTexture.width + " Y" + inputTexture.height;
+		}
+	}
+}

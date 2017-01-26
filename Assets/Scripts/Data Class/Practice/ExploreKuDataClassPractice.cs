065fb6cfe97577aeef32a423c3bfd961c5aa64b7
hluo26 <h965l741@ku.edu>
2017-01-26 10:05:19 -0600

    SimpleJSON and some practice

---
+++ Assets/Scripts/Data Class/Practice/ExploreKuDataClassPractice.cs
@@ -0,0 +1,55 @@
+﻿class Location {
+	public int id;
+	public double latitude;
+	public int location_id;
+	public string location_type;
+	public double longtitude;
+	public string name;
+}
+
+class Building: Location{
+	public string address;
+	public string description;
+	public int id;
+	public string image;
+	public string[] amenities;
+	public string[] department;
+}
+
+class Amenity{
+	public int building_id;
+	public int id;
+	public string name;
+}
+
+class Department{
+	public int building_id;
+	public int id;
+	public string name;
+}
+
+class ParkingLot: Location{
+	public int id;
+	public string status;
+}
+
+class BusStop: Location{
+	public int id;
+	public int number;
+	public string[] RouteAssignment;
+}
+
+class RouteAssignment{
+	public int bus_stop_id;
+	public int id;
+	public string route_id;
+}
+
+class Route{
+	public string direction;
+	public int id;
+	public string name;
+	public int number;
+	public string[] RouteAssignment;
+	public string[] BusStop;
+}
---
+++ Assets/Scripts/Networking/ExploreKuNetworkingTool.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExploreKuNetworkingTool : MonoBehaviour {
+
+	public void UploadJsonToUri(string json, string uri)
+	{
+		Debug.Log (json);
+		WWWForm form = new WWWForm ();
+	}
+
+}
